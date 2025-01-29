a5dc35ba010eaa1bd80e00ebeb86f9c37e035f99
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-01-29 18:03:02 -0300

    feat: add method __str__

--- movies/models.py
+++ movies/models.py
@@ -9,3 +9,6 @@ class Movie(models.Model):
     )
     director = models.CharField(null=True, max_length=100)
     winner_oscar = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} - {self.rating}"
