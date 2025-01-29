c0eed860f112b5473e724f105c0045e24d5d1afc
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-01-29 17:53:48 -0300

    feat: update class Movie adding new fields and validators

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=60)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    director = models.CharField(null=True, max_length=100)
+    winner_oscar = models.BooleanField(default=False)
