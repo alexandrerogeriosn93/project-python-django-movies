5f56ec9538de8cf270ba27c79f86f8c5a9cba072
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-01-29 19:36:28 -0300

    feat: create class Address

--- movies/models.py
+++ movies/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street} - {self.postal_code} - {self.city}"
+
+
 class Director(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
