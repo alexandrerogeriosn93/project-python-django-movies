c7c16ebee29529b3e6a2d4618bfa9a8cc8534eff
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-01-29 19:24:39 -0300

    feat: add method __str__

--- movies/models.py
+++ movies/models.py
@@ -6,6 +6,9 @@ class Director(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=60)
