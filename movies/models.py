cb73b22510819cc934d1435142151a863e83fe48
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-01-29 18:57:53 -0300

    feat: create class Director and add mapping OnyToMany

--- movies/models.py
+++ movies/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=60)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    director = models.CharField(null=True, max_length=100)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
     winner_oscar = models.BooleanField(default=False)
 
     def __str__(self):
